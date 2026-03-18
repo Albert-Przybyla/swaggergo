36d047d637d16a2fdff273cff6ab08ec447da363
Albert Przybyła <albert.przybyla2@gmail.com>
2026-03-18 10:06:58 +0100

    fix: go install version fallback

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/Albert-Przybyla/swaggergo/internal/build"
 	"github.com/spf13/cobra"
@@ -11,11 +12,34 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show CLI version info",
 	Run: func(cmd *cobra.Command, args []string) {
+		version := build.Version
+		commit := build.Commit
+		date := build.Date
+
+		if info, ok := debug.ReadBuildInfo(); ok {
+			if version == "dev" {
+				version = info.Main.Version
+			}
+
+			for _, s := range info.Settings {
+				switch s.Key {
+				case "vcs.revision":
+					if commit == "none" {
+						commit = s.Value
+					}
+				case "vcs.time":
+					if date == "unknown" {
+						date = s.Value
+					}
+				}
+			}
+		}
+
 		fmt.Printf(
 			"Version: %s\nCommit: %s\nDate: %s\n",
-			build.Version,
-			build.Commit,
-			build.Date,
+			version,
+			commit,
+			date,
 		)
 	},
 }
